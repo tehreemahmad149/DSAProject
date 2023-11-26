e6a26e7360d8c375ef84d3fe6ad821380e1136e8
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2023-11-26 13:02:22 +0500

    Update forwardIndex Class

--- forwardIndex.py
+++ forwardIndex.py
@@ -1,19 +1,13 @@
-# forward_index.py
-
+# forwardIndex.py
 class ForwardIndex:
     def __init__(self):
         self.index = {}
 
-    def add_document(self, doc_id, terms):
-        for term in terms:
-            if term in self.index:
-                self.index[term].append(doc_id)
-            else:
-                self.index[term] = [doc_id]
+    def add_document(self, doc_id, keywords):
+        self.index[doc_id] = keywords
 
-    def get_documents_for_term(self, term):
-        return self.index.get(term, [])
+    def get_keywords_for_document(self, doc_id):
+        return self.index.get(doc_id, [])
 
-    def get_original_document_id(self, ranked_document_id):
-        # Implement as needed based on your actual requirements
-        pass
+    def get_all_documents(self):
+        return list(self.index.keys())

9b1bf6f1107c64fe9605e0f87b91b3a347cdd893
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2023-11-29 00:53:32 +0500

    updated to add basic lexicon implementation

--- forwardIndexImplementation.py
+++ forwardIndexImplementation.py
@@ -32,9 +32,8 @@ def extract_content_and_id_from_json(file_path):
                         # Process content using a generator
                         tokens = process_content_generator(content_item)
 
-                        yield article_id, tokens
-            else:
-                print(f"No content found in file {file_path}")
+                        # Add document to the forward index
+                        forward_index.add_document(article_id, tokens)
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON in file {file_path}: {e}")
 
@@ -42,24 +41,36 @@ def extract_content_and_id_from_json(file_path):
 for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
     if os.path.isfile(file_path) and filename.endswith('.json'):
-        for article_id, content_tokens in extract_content_and_id_from_json(file_path):
-            forward_index.add_document(article_id, content_tokens)
+        extract_content_and_id_from_json(file_path)
 
 # Save the forward index to a TXT file with improved formatting
 output_file_path_txt = 'forward_index.txt'
 
 with open(output_file_path_txt, 'w', encoding='utf-8') as txtfile:
-    # Write header
+    # Write header for the forward index file
     txtfile.write("Document ID\tKeywords\n")
 
-    # Write each document ID and its associated keywords
+    # Write each document ID, its associated keywords
     for doc_id, keywords in forward_index.index.items():
         keyword_str = ', '.join(map(str, keywords))
         txtfile.write(f"Document ID: {doc_id}\n")
         txtfile.write(f"Keywords: {keyword_str}\n")
+
         # Add a separator line
         txtfile.write('-' * 40 + '\n')
         # Skip 3 lines
         txtfile.write('\n' * 3)
 
+# Save the lexicon to a separate TXT file
+lexicon_file_path_txt = 'lexicon.txt'
+with open(lexicon_file_path_txt, 'w', encoding='utf-8') as lexfile:
+    # Write header for the lexicon file
+    lexfile.write("Word ID\tWord\n")
+
+    # Write lexicon to the lexicon file in sorted order
+    for word in forward_index.lexicon:
+        word_id = forward_index.get_word_id(word)
+        lexfile.write(f"{word_id}\t{word}\n")
+
 print(f"Forward index saved to {output_file_path_txt}")
+print(f"Lexicon saved to {lexicon_file_path_txt}")

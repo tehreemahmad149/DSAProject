408fa5d0a1361863829a0095a803ed07202b4ad1
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2023-12-01 00:16:48 +0500

    integrated forward index in a proper format

--- class_forwardIndex.py
+++ class_forwardIndex.py
@@ -1,32 +1,41 @@
-# forwardIndex.py
+# Definition of ForwardIndex Class
 class ForwardIndex:
     def __init__(self):
-        self.index = {}
-        self.lexicon = []
-        self.word_id_mapping = {}
-        self.next_word_id = 1
+        # Initialize attributes
+        self.index = {}         # Document ID to keywords mapping
+        self.lexicon = set()     # Set of unique keywords in the entire dataset
+        self.word_id_mapping = {}  # Mapping of keywords to their corresponding word IDs
+        self.next_word_id = 1    # Counter for generating unique word IDs
 
     def add_document(self, doc_id, keywords):
+        # Add document to the index with associated keywords
         self.index[doc_id] = keywords
 
-        for word in keywords:
-            if word not in self.word_id_mapping:
-                self.word_id_mapping[word] = self.next_word_id
-                self.next_word_id += 1
-                self.lexicon.append(word)
+        # Update lexicon with new unique words from the document
+        new_words = set(keywords) - self.lexicon
+        self.lexicon.update(new_words)
+
+        # Assign word IDs to new words in the document and update word_id_mapping
+        for word in new_words:
+            self.word_id_mapping[word] = self.next_word_id
+            self.next_word_id += 1
 
     def get_keywords_for_document(self, doc_id):
+        # Return keywords associated with a specific document ID
         return self.index.get(doc_id, [])
 
     def get_original_document_id(self, ranked_document_id):
-        # Implement as needed based on your actual requirements
+        # Function for retrieving original document ID based on ranking (not yet implemented)
         pass
 
     def get_lexicon(self):
+        # Return the lexicon
         return list(self.lexicon)
 
     def get_word_id(self, word):
+        # Return the word ID for a given keyword
         return self.word_id_mapping.get(word, None)
 
     def get_word_id_mapping(self):
+        # Return the entire word_id_mapping
         return self.word_id_mapping
--- implementation_forwardIndex.py
+++ implementation_forwardIndex.py
@@ -1,76 +1,73 @@
-# forwardIndexImplementation.py
 import json
 import os
 import sys
 from class_forwardIndex import ForwardIndex
 from utils.utils import process_content_generator, generate_unique_doc_id
 
-# Set the default encoding to 'utf-8'
+# Set the default encoding for standard output to 'utf-8'
 sys.stdout.reconfigure(encoding='utf-8')
 
 # Set console encoding to 'utf-8' (for Windows)
 os.system('chcp 65001')
 
+# Create an instance of the ForwardIndex class
 forward_index = ForwardIndex()
 
-# Path to the folder containing the JSON files locally
-folder_path = '/home/gosal/tehreem-s-DSAprojectRepo/testFiles/'
+folder_path = 'C:\\Users\\user\\Documents\\GitHub\\DSAProject\\testFiles'
 
-# Function to extract content from all objects in each JSON file
+# Function to extract content and document IDs from each JSON file
 def extract_content_and_id_from_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         try:
+            # Load JSON data from the file
             data = json.load(file)
+            
             if isinstance(data, list):
+                # Iterate over each object in the list
                 for obj_index, obj in enumerate(data):
+                    # Extract content from the 'content' field of the object
                     content_item = obj.get('content', '')
+                    
+                    # Process content and generate a unique document ID
                     if content_item:
-                        # Generate a unique document ID
                         file_name = os.path.splitext(os.path.basename(file_path))[0]
                         article_id = generate_unique_doc_id(file_name, obj_index)
-
-                        # Process content using a generator
                         tokens = process_content_generator(content_item)
-
-                        # Add document to the forward index
+                        
+                        # Add the document to the forward index
                         forward_index.add_document(article_id, tokens)
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON in file {file_path}: {e}")
 
-# Iterating over each file in the folder for the forward index implementation
+# Iterate over each file
 for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
+    # Check if the item is a file and has a '.json' extension
     if os.path.isfile(file_path) and filename.endswith('.json'):
         extract_content_and_id_from_json(file_path)
 
-# Save the forward index to a TXT file with improved formatting
 output_file_path_txt = 'forward_index.txt'
 
+# Write the forward index to a TXT file (line by line)
 with open(output_file_path_txt, 'w', encoding='utf-8') as txtfile:
-    # # Write header for the forward index file
-    # txtfile.write("Document ID\tKeywords\n")
-
-    # Write each document ID, its associated keywords
+    # Iterate over each document in the forward index
     for doc_id, keywords in forward_index.index.items():
         keyword_str = ', '.join(map(str, keywords))
+        # Write document ID and associated keywords to the output file
         txtfile.write(f"Document ID: {doc_id}\n")
         txtfile.write(f"Keywords: {keyword_str}\n")
 
-        # Add a separator line
-        txtfile.write('-' * 40 + '\n')
-        # Skip 3 lines
-        txtfile.write('\n' * 3)
-
-# Save the lexicon to a separate TXT file
 lexicon_file_path_txt = 'lexicon.txt'
-with open(lexicon_file_path_txt, 'w', encoding='utf-8') as lexfile:
-    # Write header for the lexicon file
-    lexfile.write("Word ID\tWord\n")
 
-    # Write lexicon to the lexicon file in sorted order
-    for word in forward_index.lexicon:
+# Writing the lexicon to a TXT file in sorted order based on word IDs
+with open(lexicon_file_path_txt, 'w', encoding='utf-8') as lexfile:
+    lexfile.write("Word ID\tWord\n")
+    sorted_lexicon = sorted(forward_index.get_lexicon(), key=forward_index.get_word_id)
+    # Iterate over each word in the lexicon
+    for word in sorted_lexicon:
         word_id = forward_index.get_word_id(word)
         lexfile.write(f"{word_id}\t{word}\n")
 
+# Display success messages
 print(f"Forward index saved to {output_file_path_txt}")
 print(f"Lexicon saved to {lexicon_file_path_txt}")
